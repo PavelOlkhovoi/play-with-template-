aa1a316988eaf4d17d8bd11ea94aff105252ad70
Pavel <p-olhovoi@bk.ru>
2023-11-02 17:54:45 +0100

    issue #094 add mock of history graph

--- src/nodes-edges.js
+++ src/nodes-edges.js
@@ -5,66 +5,34 @@ export const initialNodes = [
   {
     id: "1",
     type: "input",
-    data: { label: "input" },
+    data: { label: "Barmen 3 153/25" },
     position,
   },
   {
     id: "2",
-    data: { label: "node 2" },
-    position,
-  },
-  {
-    id: "2a",
-    data: { label: "node 2a" },
-    position,
-  },
-  {
-    id: "2b",
-    data: { label: "node 2b" },
-    position,
-  },
-  {
-    id: "2c",
-    data: { label: "node 2c" },
-    position,
-  },
-  {
-    id: "2d",
-    data: { label: "node 2d" },
+    data: { label: "Barmen 3 496/0" },
     position,
   },
   {
     id: "3",
-    data: { label: "node 3" },
+    data: { label: "Barmen 3 498/0" },
     position,
   },
   {
     id: "4",
-    data: { label: "node 4" },
+    data: { label: "Barmen 3 497/0" },
     position,
   },
   {
     id: "5",
-    data: { label: "node 5" },
+    data: { label: "Barmen 3 501/0" },
     position,
   },
-  {
-    id: "6",
-    type: "output",
-    data: { label: "output" },
-    position,
-  },
-  { id: "7", type: "output", data: { label: "output" }, position },
 ];
 
 export const initialEdges = [
   { id: "e12", source: "1", target: "2", type: edgeType, animated: true },
   { id: "e13", source: "1", target: "3", type: edgeType, animated: true },
-  { id: "e22a", source: "2", target: "2a", type: edgeType, animated: true },
-  { id: "e22b", source: "2", target: "2b", type: edgeType, animated: true },
-  { id: "e22c", source: "2", target: "2c", type: edgeType, animated: true },
-  { id: "e2c2d", source: "2c", target: "2d", type: edgeType, animated: true },
-  { id: "e45", source: "4", target: "5", type: edgeType, animated: true },
-  { id: "e56", source: "5", target: "6", type: edgeType, animated: true },
-  { id: "e57", source: "5", target: "7", type: edgeType, animated: true },
+  { id: "e22a", source: "1", target: "4", type: edgeType, animated: true },
+  { id: "e22b", source: "3", target: "5", type: edgeType, animated: true },
 ];
