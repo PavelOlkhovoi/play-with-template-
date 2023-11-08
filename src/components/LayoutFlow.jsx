d68c458864b8112c4fd5f2b88324142941da9360
Pavel <p-olhovoi@bk.ru>
2023-11-08 12:47:06 +0100

    add example with dagree

--- src/components/LayoutFlow.jsx
+++ src/components/LayoutFlow.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
-import ReactFlow from "reactflow";
+import ReactFlow, {
+  addEdge,
+  ConnectionLineType,
+  Panel,
+  useNodesState,
+  useEdgesState,
+  Controls,
+} from "reactflow";
+import dagre from "dagre";
 
 import "reactflow/dist/style.css";
 
@@ -9,46 +17,121 @@ const initialNodes = [
   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-export const LayoutFlow = ({
-  backgroundColor = "blue",
-  initialNodes = initialNodes,
-  initialEdges = initialEdges,
-}) => {
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const getLayoutedElements = (nodes, edges, direction = "TB") => {
+  const isHorizontal = direction === "LR";
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? "left" : "top";
+    node.sourcePosition = isHorizontal ? "right" : "bottom";
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
+export const LayoutFlow = ({ backgroundColor = "blue" }) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+  const onConnect = useCallback(
+    (params) =>
+      setEdges((eds) =>
+        addEdge(
+          { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+          eds
+        )
+      ),
+    []
+  );
+  const onLayout = useCallback(
+    (direction) => {
+      const { nodes: layoutedNodes, edges: layoutedEdges } =
+        getLayoutedElements(nodes, edges, direction);
+
+      setNodes([...layoutedNodes]);
+      setEdges([...layoutedEdges]);
+    },
+    [nodes, edges]
+  );
+
   return (
     <div
       style={{
-        width: "400px",
-        height: "400px",
+        width: "800px",
+        height: "800px",
         backgroundColor: backgroundColor,
         padding: "1rem",
       }}
     >
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        fitView
+      >
+        <Controls />
+        <Panel position="top-right">
+          <button onClick={() => onLayout("TB")}>vertical layout</button>
+          <button onClick={() => onLayout("LR")}>horizontal layout</button>
+        </Panel>
+      </ReactFlow>
     </div>
   );
 };
 
 LayoutFlow.propTypes = {
   backgroundColor: PropTypes.string,
-  initialNodes: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      position: PropTypes.shape({
-        x: PropTypes.number,
-        y: PropTypes.number,
-      }),
-      data: PropTypes.shape({
-        label: PropTypes.string,
-      }),
-    })
-  ),
-  initialEdges: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      source: PropTypes.string,
-      target: PropTypes.string,
-    })
-  ),
+  //   initialNodes: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.string,
+  //       position: PropTypes.shape({
+  //         x: PropTypes.number,
+  //         y: PropTypes.number,
+  //       }),
+  //       data: PropTypes.shape({
+  //         label: PropTypes.string,
+  //       }),
+  //     })
+  //   ),
+  //   initialEdges: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.string,
+  //       source: PropTypes.string,
+  //       target: PropTypes.string,
+  //     })
+  //   ),
 };
 
 LayoutFlow.defaultProps = {
