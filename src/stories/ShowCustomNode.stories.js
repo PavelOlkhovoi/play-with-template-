a9181f9b81d32eb0aabecef6ceb9593c17e88e17
Pavel <p-olhovoi@bk.ru>
2023-11-22 23:31:16 +0100

    add TextUpdaterNode

---
+++ src/stories/ShowCustomNode.stories.js
@@ -0,0 +1,10 @@
+import { ShowCustomNode } from "../components/custom-nodes/ShowCustomNode";
+export default {
+  component: ShowCustomNode,
+};
+
+export const Simple = {
+  args: {
+    backgroundColor: "white",
+  },
+};
