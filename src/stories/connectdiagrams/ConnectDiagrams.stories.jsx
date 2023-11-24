ab7b97c3e7e86bcd554541438dd5aa207cd799ee
Pavel <p-olhovoi@bk.ru>
2023-11-24 10:26:04 +0100

    add connect storis diagrams

---
+++ src/stories/connectdiagrams/ConnectDiagrams.stories.jsx
@@ -0,0 +1,212 @@
+import React, { useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  Background,
+  useNodesState,
+  useEdgesState,
+  MarkerType,
+  Handle,
+  Position,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+import { ConsoleSqlOutlined, CodeOutlined } from "@ant-design/icons";
+import { vzkat } from "./data";
+export default {
+  title: "Stories/DBConnection",
+};
+
+const manualNodes = [
+  {
+    id: "1",
+    type: "rightPort",
+    position: { x: 100, y: 200 },
+    data: vzkat.vzkat_richtung,
+  },
+  {
+    id: "2",
+    type: "leftPort",
+    position: { x: 400, y: 200 },
+    data: vzkat.vzkat_richtung,
+  },
+];
+
+const simpleEdges = [
+  {
+    id: "e2-3",
+    source: "1",
+    target: "2",
+    isConnectableStart: false,
+  },
+];
+
+const reactFLowWrapperCardCss = {
+  fontFamily: '"Helvetica", sans-serif',
+  fontSize: "9px",
+  width: "180px",
+  background: "white",
+  color: "#222",
+  boxShadow: "0 4px 6px -1px rgb(0 0 0 / 15%), 0 2px 4px -1px rgb(0 0 0 / 8%)",
+  border: "1px solid #000000",
+  borderRadius: "8px",
+};
+
+const headerCardCss = {
+  backgroundColor: "#e5e7eb",
+  borderBottom: "1px solid #000000",
+  padding: "8px 8px",
+  borderRadius: "8px 8px 0px 0px",
+};
+
+const headerCardTitleCss = {
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+
+const rowClass = {
+  padding: "4px 8px",
+};
+
+const rowFirstItemClass = { ...rowClass, marginTop: "4px" };
+const rowLastItemClass = { ...rowClass, marginBottom: "4px" };
+
+const rowWrapperCss = {
+  width: "100%",
+  borderCollapse: "collapse",
+  border: "1px solid #000000",
+  fontSize: "8px",
+  borderRadius: "8px",
+  overflow: "hidden",
+};
+
+const rowCardWithBorder = { padding: "4px 8px" };
+const cardBodyGray = {
+  backgroundColor: "#e0e0e0",
+  margin: "0",
+  padding: "1px 0px",
+  borderRadius: "0px 0px 8px 8px",
+};
+
+const PortRighttWhiteBorderless = ({ id, data }) => {
+  const iconStyle = { fontSize: "8px" };
+  return (
+    <div style={{ ...reactFLowWrapperCardCss, border: "0" }}>
+      <div style={headerCardCss}>
+        <div style={headerCardTitleCss}>
+          <span style={{ alineText: "left" }}>{data.name}</span>
+          <ConsoleSqlOutlined style={iconStyle} />
+        </div>
+      </div>
+      <div style={{ ...cardBodyGray, background: "white" }}>
+        {Object.keys(data.attributes).map((key, index) => {
+          const item = data.attributes[key];
+          return (
+            <div
+              style={
+                index === 0
+                  ? rowFirstItemClass
+                  : index === Object.keys(data.attributes).length - 1
+                  ? rowLastItemClass
+                  : rowClass
+              }
+              key={index}
+            >
+              {item.name === "id" ? (
+                <div style={{ position: "relative" }}>
+                  {item.name}
+                  <Handle
+                    type="source"
+                    position={Position.Right}
+                    id={id}
+                    style={{
+                      top: 5,
+                      right: -12,
+                      background: "#494949",
+                      border: "1px solid #e5e7eb",
+                    }}
+                  />
+                </div>
+              ) : (
+                <div>{item.name}</div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+const PortLeftWhiteBorderless = ({ id, data }) => {
+  const iconStyle = { fontSize: "8px" };
+  return (
+    <div style={{ ...reactFLowWrapperCardCss, border: "0" }}>
+      <div style={headerCardCss}>
+        <div style={headerCardTitleCss}>
+          <span style={{ alineText: "left" }}>{data.name}</span>
+          <ConsoleSqlOutlined style={iconStyle} />
+        </div>
+      </div>
+      <div style={{ ...cardBodyGray, background: "white" }}>
+        {Object.keys(data.attributes).map((key, index) => {
+          const item = data.attributes[key];
+          return (
+            <div
+              style={
+                index === 0
+                  ? rowFirstItemClass
+                  : index === Object.keys(data.attributes).length - 1
+                  ? rowLastItemClass
+                  : rowClass
+              }
+              key={index}
+            >
+              {item.name === "id" ? (
+                <div style={{ position: "relative" }}>
+                  {item.name}
+                  <Handle
+                    type="source"
+                    position={Position.Left}
+                    id={id}
+                    style={{
+                      top: 5,
+                      left: -12,
+                      background: "#494949",
+                      border: "1px solid #e5e7eb",
+                    }}
+                  />
+                </div>
+              ) : (
+                <div>{item.name}</div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export const FirstCOnnection = () => {
+  return (
+    <div
+      style={{
+        width: "1300px",
+        height: "650px",
+        backgroundColor: "white",
+        padding: "1rem",
+      }}
+    >
+      <ReactFlow
+        nodes={manualNodes}
+        edges={simpleEdges}
+        fitView
+        nodeTypes={{
+          rightPort: PortRighttWhiteBorderless,
+          leftPort: PortLeftWhiteBorderless,
+        }}
+      />
+    </div>
+  );
+};
